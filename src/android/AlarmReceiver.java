afa78c35c70142bb48e102c76a8e6967803b5718
dbessa1 <daniel.de.bessa@gmail.com>
2017-08-11 18:20:39 +0000

    Update AlarmReceiver.java

--- src/android/AlarmReceiver.java
+++ src/android/AlarmReceiver.java
@@ -21,14 +21,14 @@ public class AlarmReceiver extends BroadcastReceiver {
 */
         PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
         WakeLock wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
-        wakeLock.acquire();
+        //wakeLock.acquire();
  
         KeyguardManager keyguardManager = (KeyguardManager)context.getSystemService(Context.KEYGUARD_SERVICE); 
         KeyguardLock keyguardLock =  keyguardManager.newKeyguardLock("TAG");
         keyguardLock.disableKeyguard();
 
         Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(2000);
+        v.vibrate(1000);
    
         intent = new Intent();
         intent.setAction("com.uniclau.alarmplugin.ALARM");
