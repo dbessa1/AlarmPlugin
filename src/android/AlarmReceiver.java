9345465a3bfc8c2c4b00f9b52d2b2c45edf48d88
dbessa1 <daniel.de.bessa@gmail.com>
2017-08-11 23:16:55 +0000

    Update AlarmReceiver.java

--- src/android/AlarmReceiver.java
+++ src/android/AlarmReceiver.java
@@ -28,16 +28,15 @@ public class AlarmReceiver extends BroadcastReceiver {
         keyguardLock.disableKeyguard();
 
         Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(1000);
+        v.vibrate(500);
    
         //System.exit(0);
         
         intent = new Intent();
         intent.setAction("com.uniclau.alarmplugin.ALARM");
         intent.setPackage(context.getPackageName());
-        intent.addFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
-        
-       
+
     }
 }
